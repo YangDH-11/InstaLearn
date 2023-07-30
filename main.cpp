b1637673d4fc2fbec7d8aba4c2ec41fee3d737b4
YangDH-11 <3218526163@qq.com>
2023-07-30 11:50:43 +0800

    <Fix>: modify TwoNumAdd, check if result is true

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int TwoNumAdd(int a, int b){
-  cout << "add two num" << endl;
-  return (a + b);
+  cout << "add two num!" << endl;
+  if((INT_MAX - a) < b || (INT_MIN - a) > b)
+  {
+    cout << "result out of range!" << endl;
+    return ((INT_MAX - a) < b) ? INT_MAX : INT_MIN;
+  }
+  int res = a + b;
+  return res;
 }
 
 int main(int argc, char *argv[]) {
